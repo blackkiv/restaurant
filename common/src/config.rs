2050c25cf9701d9380adb727b4ffd066a0210ad3
blackkiv <fallena238@gmail.com>
2022-08-13 17:54:45 +0300

    rustfmt

--- common/src/config.rs
+++ common/src/config.rs
@@ -4,8 +4,8 @@ use std::fs;
 use serde::Deserialize;
 
 pub fn load_config<T>() -> &'static T
-    where
-        T: for<'a> Deserialize<'a> + Debug,
+where
+    T: for<'a> Deserialize<'a> + Debug,
 {
     let config_source = fs::read_to_string("resources/config.toml").expect("config file not found");
     let config = toml::from_str(&config_source).expect("wrong config file format");
