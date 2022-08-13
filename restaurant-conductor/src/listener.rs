e0ab3d83e4835c163c05d6c6f5c75bec25332316
blackkiv <fallena238@gmail.com>
2022-08-13 15:18:43 +0300

    fix listener in restaurant-conductor

--- restaurant-conductor/src/listener.rs
+++ restaurant-conductor/src/listener.rs
@@ -4,7 +4,7 @@ use tokio::join;
 use tokio::sync::Mutex;
 
 use common::KafkaConsumer;
-use common::model::Recipe;
+use common::model::{Order, Recipe};
 use common::types::EmptyStaticResult;
 
 use crate::Config;
@@ -38,18 +38,9 @@ pub async fn listen_events(config: &Config, collection: &'static Arc<Mutex<Mongo
         Ok(())
     };
     let order_prepared_consumer = async move |row_event: Vec<u8>| -> EmptyStaticResult {
-        let recipe = serde_json::from_slice::<Recipe>(row_event.as_slice())
-            .map_err(|err| err.to_string())?;
-        println!("order prepared event received {:?}", recipe);
-        let recipe_hash = recipe.hash.clone();
-        collection
-            .lock()
-            .await
-            .recipe_collection
-            .save(recipe)
-            .await
-            .map_err(|err| err.to_string())?;
-        println!("order {} saved", recipe_hash);
+        let order =
+            serde_json::from_slice::<Order>(row_event.as_slice()).map_err(|err| err.to_string())?;
+        println!("order prepared event received {:?}", order);
         Ok(())
     };
 
